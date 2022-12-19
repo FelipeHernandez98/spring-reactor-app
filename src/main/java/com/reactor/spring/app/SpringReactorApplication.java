668accf61d51400c6178cb4af1b2fe9133fa1b60
Andres Felipe Hernandez Caicedo <afhernandez@personalsoft.com.co>
2022-12-19 17:04:03 -0500

    creando un flux y viendo las dos maneras de imprimir los elementos del flux

--- src/main/java/com/reactor/spring/app/SpringReactorApplication.java
+++ src/main/java/com/reactor/spring/app/SpringReactorApplication.java
@@ -1,5 +1,7 @@
 package com.reactor.spring.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,17 +10,28 @@ import reactor.core.publisher.Flux;
 @SpringBootApplication
 public class SpringReactorApplication implements CommandLineRunner {
 
+	private static final Logger log = LoggerFactory.getLogger(SpringReactorApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringReactorApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-	 Flux<String> nombres = Flux.just("Andres", "Felipe", "Angie", "Liceth")
-			 .doOnNext( nomb ->  {
-				 System.out.println(nomb);
-			 });
 
-	 nombres.subscribe();
+		Flux<String> nombres = Flux.just("Andres", "Felipe", "Angie", "Liceth")
+				.doOnNext(System.out::println);
+
+//                     ó
+//	 Flux<String> nombres = Flux.just("Andres", "Felipe", "Angie", "Liceth")
+//			 .doOnNext( nomb ->  {
+//				 System.out.println(nomb);
+//			 });
+
+	 nombres.subscribe(log::info); //Se resume el codigo y queda mas limpio
+//	            ó
+//	 nombres.subscribe( nomb ->{
+//		 log.info(nomb);
+//	 });
 	}
 }
